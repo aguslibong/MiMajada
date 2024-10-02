994017764585ceace0493650146d902829068c85
Agustin Li Bongiorno <164107455+aguslibong@users.noreply.github.com>
2024-10-01 22:24:11 -0300

    Delete src/Backend/model directory

--- src/Backend/model/Sexo.ts
+++
@@ -1,9 +0,0 @@
-class Sexo{
-    private descripcion: String;
-    private IdSexo: number;
-
-    constructor(descripcion: string, IdSexo: number) {
-        this.descripcion = descripcion;
-        this.IdSexo = IdSexo;
-    }
-}
